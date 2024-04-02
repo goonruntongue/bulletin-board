cb02f7e2078079859b5aa3f4dc897f17215950b2
goonruntongue <68429888+goonruntongue@users.noreply.github.com>
2024-04-02 15:56:17 +0900

    XSSの防止

--- simple-memo/read-write.php
+++ simple-memo/read-write.php
@@ -4,7 +4,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") :
     $timestamp = date("Y年m月d日h時i分s秒");
     $datetime = date("Y-m-d-h-i-s");
     $file = "data/data-{$timestamp}.txt";
-    $comment = "<p class='comment' data-timestamp='{$timestamp}'>" . "<time datetime='{$datetime}'>投稿日：{$timestamp}</time>" . nl2br(str_replace(" ", "&nbsp;", $_POST["comment"])) . "</p>";
+    function esc_html($arg)
+    {
+        return htmlspecialchars($arg, ENT_QUOTES);
+    }
+    $comment = "<p class='comment' data-timestamp='{$timestamp}'>" . "<time datetime='{$datetime}'>投稿日：{$timestamp}</time>" . nl2br(str_replace(" ", "&nbsp;", esc_html($_POST["comment"]))) . "</p>";
     file_put_contents($file, $comment, FILE_APPEND);
 endif;
 //ファイル群を配列として取得
